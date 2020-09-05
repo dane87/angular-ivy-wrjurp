e388a6b534c52618db70dffe7f6acc0bf9de70d6
dane87 <daneventer@gmail.com>
2020-09-05 21:40:54 +0200

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, AppRoutingModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
