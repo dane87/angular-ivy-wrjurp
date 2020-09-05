c198f42dfe57560c75e4164b83524ef09f5d3a47
dane87 <daneventer@gmail.com>
2020-09-05 21:53:50 +0200

    3 files modified, 1 added

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,5 @@ import './polyfills';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
-
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
