7388908971cb8918d3c3665c82f1124b6a1ae546
dane87 <daneventer@gmail.com>
2020-09-05 21:58:49 +0200

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import './polyfills';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
+
+enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
